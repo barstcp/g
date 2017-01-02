0124524452634fee8032b5ccf8a281754a1fc5cb
rama333 <ramil-faxrut@yandex.ru>
2017-01-02 16:01:35 +0400

    add new project

---
+++ app/src/main/java/com/example/perm/perm_rest_union/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.perm.perm_rest_union;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.perm.perm_rest_union.view.fragments.LoginFragment;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  final static String TAG = "TAG";
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentByTag(TAG);
+        if (fragment == null) replaceFragment(new LoginFragment(), false);
+    }
+
+    private void replaceFragment(Fragment fragment, boolean addBackStack) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment, TAG);
+        if (addBackStack) transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/perm/perm_rest_union/view/fragments/LoginFragment.java
@@ -0,0 +1,23 @@
+package com.example.perm.perm_rest_union.view.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.perm.perm_rest_union.R;
+
+/**
+ * Created by Ramil on 24.12.2016.
+ */
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+        View view = layoutInflater.inflate(R.layout.login_fragment, viewGroup, false);
+
+        return view;
+    }
+}
